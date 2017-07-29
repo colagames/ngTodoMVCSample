03450d6944e8b540c4ccf8917e1c306fd4e2324b
yihsin <cheriish2001@hotmail.com>
2017-07-29 13:19:14 +0800

    建立 footer-component.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponentComponent } from './title-component/title-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponentComponent
+    TitleComponentComponent,
+    FooterComponentComponent
   ],
   imports: [
     BrowserModule,
