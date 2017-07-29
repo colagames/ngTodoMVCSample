84392ef2894359cda332de1b4c6f3e27090ed672
yihsin <cheriish2001@hotmail.com>
2017-07-29 11:48:54 +0800

    任務 07：套用雙向繫結 (Two-way Binding)
    任務說明
    
    在 AppComponent 元件中新增一個 todo 屬性 (型別為字串)
    設定 <input> 與 todo 屬性雙向繫結，並用 Augury 查看 AppComponent 的狀態
    在按下 Enter 並新增到 todos 陣列後，輸入框上已經輸入的文字必須清除
    任務時間
    
    預計在 3 分鐘之內可完成
    參考連結
    
    Template Syntax - ts - GUIDE
    進階練習
    
    試著將雙向繫結修改為兩個單向繫結 ( 屬性繫結 + 事件繫結 )
    [(x)] 語法事實上是 [x] 與 (xChange) 的結合 (語法糖)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,17 @@ export class AppComponent {
   // title = 'app'; 任務 04 : 將 app.component.ts 中的 title 屬性移到 title.component.ts 檔案中
 
   inputHint = '你有什麼待辦事項嗎?';
-  colspan = 2;
+
+  todos: any[] = []; // 需要給初始值，空陣列
+  todo = '';
+
+
+  addTodos() {
+    console.log(this.todo);
+    this.todos.push(this.todo);
+    this.todo = '';
+  }
+
+
+
 }
