f170ae32672e420f36882fdfb6e6ec2197458afa
yihsin <cheriish2001@hotmail.com>
2017-07-29 15:01:05 +0800

    移除 ngOnChanges() 方法，改用 TypeScript 的 getter/setter 來自訂 todos 的傳入方式 改用 setter 來套用 @Input() 裝飾器 (decorator) 當傳入 FooterComponent 的 todos 資料筆數大於 5 項時，要將 5 item left 透過 ngStyle 修改為紅色粗體

--- src/app/footer-component/footer-component.component.ts
+++ src/app/footer-component/footer-component.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-footer-component',
   templateUrl: './footer-component.component.html',
   styleUrls: ['./footer-component.component.css'],
 })
-export class FooterComponentComponent implements OnInit, OnChanges {
+export class FooterComponentComponent implements OnInit {
 
 
   tooMore = false;
 
 
-  @Input() todos: any[] = [];
+  // @Input() todos: any[] = [];
+  private _todos: any[] = [];
+
+  @Input()
+  set todos(value) {
+    this._todos = value;
+    this.tooMore = this.todos.length > 5;
+  }
+  get todos() {
+    return this._todos;
+  }
 
   @Output() clearComplete = new EventEmitter();
 
@@ -25,9 +35,6 @@ export class FooterComponentComponent implements OnInit, OnChanges {
     this.clearComplete.emit();
   }
 
-  ngOnChanges() {
-    console.log('ngOnChanges');
-    this.tooMore = this.todos.length > 5;
-  }
+
 
 }
