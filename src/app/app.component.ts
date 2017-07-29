dffb2025bd1214878cab2a2a5d961ed7e450ef7f
yihsin <cheriish2001@hotmail.com>
2017-07-29 10:55:27 +0800

    任務 05：套用屬性繫結 (Property Binding)
    任務說明
    
    在 AppComponent 中建立一個 inputHint 屬性
    在 app.component.html 中使用屬性繫結套用 inputHint 到 <input> 的 placeholder 屬性裡
    任務時間
    
    預計在 1 分鐘之內可完成
    參考連結
    
    Template Syntax - ts - GUIDE
    進階練習
    
    試著將屬性繫結的中括號 [ 與 ] 移除，看看會出現甚麼錯誤
    新增一個 table 在頁面中 (如下)，試著用屬性繫結將 colspan 修改為從 AppComponent 元件中取得
          <table>
            <tr>
              <td colspan="2">Two Column</td>
              <td>One Column</td>
            </tr>
          </table>
    試著在 <input> 加上一個自訂的 HTML 擴充屬性 ( data-myhint )
    HTML 範例：<input data-myhint="TEST DATA">
    將這個 HTML5 data-myhint 擴充屬性內容設定為 AppComponent 內的 inputHint 屬性內容
    請用 Angular 2 屬性繫結 (Property Binding) 來完成任務
    請用 Google Chrome 的開發者工具查看結果是否正確
    注意事項
    
    避免副作用
    使用內嵌繫結絕對不應該修改任何屬性或對任何元件、屬性進行更新動作！
    要特別注意傳入 [屬性] 的物件型別必須與元件能傳入的物件型別一致
    只有符合以下條件，使用屬性繫結時可以不用加上 [ 與 ]
    目標屬性可以接受「字串」型別的物件
    傳入屬性的值是固定的
    傳入屬性的值之後不會再變更

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // title = 'app'; 任務 04 : 將 app.component.ts 中的 title 屬性移到 title.component.ts 檔案中
+
+  inputHint = '你有什麼待辦事項嗎?';
 }
