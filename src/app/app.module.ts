e1d6934395fce86d438a3a67f6f607eedfd08b0a
yihsin <cheriish2001@hotmail.com>
2017-07-29 10:20:35 +0800

    任務 03：切割網頁為多個子元件
    任務說明
    
    建立 TitleComponent 元件
    將 AppComponent 元件範本中的 <h1>todos</h1> 移至全新的 TitleComponent 元件中
    將 TitleComponent 元件 Directive 加入到 AppComponent 元件裡
    進階練習
    
    嘗試不同參數建立元件 ( ng help generate )
    建立不含 *.spec.ts 的單元測試程式碼
    ng g c --spec=false <ComponentName>
    不要建立元件資料夾
    ng g c --flat <ComponentName>
    建立 Inline 的樣式與範本
    ng g c --inline-style --inline-template <ComponentName>
    嘗試將上述練習建立的多餘元件從專案中移除，並確認目前專案可以在瀏覽器正常顯

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleComponentComponent } from './title-component/title-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponentComponent
   ],
   imports: [
     BrowserModule,
